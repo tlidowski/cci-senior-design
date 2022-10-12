3a7c2f8cca3058fa6575fe83fc80b1d9bed43213
Sarar Aseer <sararaseer@sarars-mbp.mynetworksettings.com>
2022-10-11 20:01:47 -0400

    api key

--- fbi_api.py
+++ fbi_api.py
@@ -9,7 +9,7 @@ import io
 def national_crime_estimate_to_from_year(start, end, key):
     base = 'https://api.usa.gov/crime/fbi/sapi/api'
     crime_last_year = '/estimates/national/' + str(start) + '/' + str(end)
-    response = requests.get(base + crime_last_year + key)
+    response = requests.get(base + crime_last_year + "?API_KEY=" + key)
     data = response.json()
     print(data)
 
@@ -17,6 +17,6 @@ def national_crime_estimate_to_from_year(start, end, key):
 def state_crime_estimate_to_from_year(state_abbrev, start, end, key):
     base = 'https://api.usa.gov/crime/fbi/sapi/api'
     summary_by_state_year = '/estimates/states/' + state_abbrev + "/" + str(start) + '/' + str(end)
-    response = requests.get(base + summary_by_state_year + key)
+    response = requests.get(base + summary_by_state_year + "?API_KEY=" + key)
     data = response.json()
     print(data)
