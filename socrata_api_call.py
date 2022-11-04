eea6a0f8929e3030f9f2924e03ca95df851874fc
Sarar Aseer <sararaseer@sarars-mbp.mynetworksettings.com>
2022-11-03 21:36:33 -0400

    zip

--- socrata_api_call.py
+++ socrata_api_call.py
@@ -7,3 +7,9 @@ def socrata_source_all(app_token, data_link, data_id):
     results = client.get_all(data_id)
     results_df = pd.DataFrame.from_records(results)
     print(results_df)
+
+def socrata_source_zip_code(app_token, data_link, data_id, zip):
+    client = Socrata(data_link, app_token)
+    results = client.get(data_id, where="zip_code="+ str(zip))
+    results_df = pd.DataFrame.from_records(results)
+    print(results_df)
