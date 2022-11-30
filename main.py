c9f185449947b0425e7e012a5500f6b6693dc623
Adrienne Cheung <arieche23@gmail.com>
2022-11-30 16:37:46 -0500

    baltimore 2020-2022, denver 2020 csvs

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import fbi_api
 import dc_parser
 import baltimore_parser
 import charlotte_parser
+import socrata_api_call
+import pandas as pd
 
 def testFbiApi():
    apiKey = "?API_KEY=zZAJhHKFBdhY3xduUAUS9uGtLrnnc33EwMIgdCcz"
@@ -16,12 +18,29 @@ def testBaltimoreParser():
 
 def testCharlotteParser():
    charlotte_parser.charlotte_incident_reports()
-   charlotte_parser.charlotte_violent_crime()
+   #charlotte_parser.charlotte_violent_crime()
 
 if __name__ == "__main__":
-   # testDcParser()
-   print("============================= BALTIMORE =============================")
-   print("\n")
-   testBaltimoreParser()
+   pd.set_option('display.max_columns', None)
+
+   token_socrata = 'T8kdBcPaIk15Bhose4cO3MDdH'
+
+   new_york_api_key_secret = 'ebmbopds1rxknoxawdvv59301damceco2420hyoo7lm0ejz98'
+   new_york_api_key_id = '3mctpgwecml66r6fig75s7r7r'
+   new_york_data_id = "uip8-fykc"
+   new_york_data_link = "data.cityofnewyork.us"
+
+
+   austin_link = 'data.austintexas.gov'
+   austin_id = "fdj4-gpfu"
+   #socrata_api_call.socrata_source_zip_code(token_socrata, austin_link, austin_id, 76574)
+   #socrata_api_call.socrata_source_all(token_socrata, new_york_data_link, new_york_data_id)
+
+   philadelphia_parser.philadelphia_data_by_year("2018")
+   #testCharlotteParser()
+   #testDcParser()
+   # print("============================= BALTIMORE =============================")
+   # print("\n")
+   #testBaltimoreParser()
 
 
