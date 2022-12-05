0b518535c50cccac4db300b9dbf4ccebe45c54b0
Sarar Aseer <sararaseer@Sarars-MacBook-Pro.local>
2022-12-05 17:29:34 -0500

    heat map

--- crime_eye/app_route.py
+++ crime_eye/app_route.py
@@ -1,3 +1,6 @@
+import json
+
+import numpy as np
 from flask import Flask, render_template, jsonify, request
 import requests
 import pandas as pd
@@ -63,6 +66,49 @@ def get_city_crime_data():
             res_json = res.to_json(orient='records')
         return res_json
     except:
+        return {}
+
+
+@app.route('/city_geo_json', methods=['GET'])
+def get_city_geo_data():
+    city = request.args.get('city')
+    start = request.args.get('start')
+    end = request.args.get('end')
+    geo_json = {"type": "FeatureCollection", "features": []}
+    try:
+        path_2020 = f'../combined_city_data/city_data-{2020}.csv'
+        path_2021 = f'../combined_city_data/city_data-{2021}.csv'
+
+        year_csvs = {"2020": path_2020,
+                     "2021": path_2021
+                     }
+
+        column_types = get_column_types()
+
+        if (start in year_csvs.keys()) and (end in year_csvs.keys()):
+            start_year_df = pd.read_csv(year_csvs[start], dtype=column_types)
+            start_year_res = apply_mask(start_year_df, 'CITY_NAME', city)
+
+            end_year_df = []
+            end_year_res = []
+
+            if start != end:
+                end_year_df = pd.read_csv(year_csvs[end], dtype=column_types)
+                end_year_res = apply_mask(end_year_df, 'CITY_NAME', city)
+
+            # res = None
+            if len(end_year_res) != 0:
+                res = pd.concat([start_year_res, end_year_res], axis=0)
+            else:
+                res = start_year_res
+            for i, row in res.iterrows():
+                if not (np.isnan(row["LONGITUDE"]) and np.isnan(row["LATITUDE"])):
+                    template = {"type": "Feature", "geometry": {"type": "Point"}}
+                    template["geometry"]["coordinates"] = [row["LONGITUDE"], row["LATITUDE"]]
+                    geo_json["features"].append(template)
+        return json.dumps(geo_json)
+    except:
+        print("Failure")
         return {}
 
 
--- crime_eye/static/loadData.js
+++ crime_eye/static/loadData.js
@@ -1,20 +1,107 @@
 let table_parent = document.getElementById("table_parent")
 let graph = document.getElementById("graph")
 let pull = document.getElementById("pull")
+let mapTab = document.getElementById("nav-graph-tab")
+let dataChanged = false
+let geoData = {"type": "FeatureCollection", "features": []};
+
+mapTab.addEventListener('shown.bs.tab', function () {
+    map.resize()
+})
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3JhODQiLCJhIjoiY2w4ZjNmcXk4MDllbDQwbnpoOXJwa2VsZSJ9.OsLldCR-T9CjYaBE5Fa4OA';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v11',
+    center: [-97.62831376, 30.28096067],
+    zoom: 11
+});
+map.on('load', () => {
+    map.addSource('crime', {
+        'type': 'geojson',
+        'data': geoData
+    });
+
+    map.addLayer(
+        {
+            'id': 'crime-heat',
+            'type': 'heatmap',
+            'source': 'crime',
+            'paint': {
+                'heatmap-intensity': {
+                    'stops': [
+                        [11, 1],
+                        [15, 3]
+                    ]
+                },
+                'heatmap-color': [
+                    'interpolate',
+                    ['linear'],
+                    ['heatmap-density'],
+                    0,
+                    'rgba(236,222,239,0)',
+                    0.2,
+                    'rgb(208,209,230)',
+                    0.4,
+                    'rgb(166,189,219)',
+                    0.6,
+                    'rgb(103,169,207)',
+                    0.8,
+                    'rgb(28,144,153)'
+                ],
+                'heatmap-radius': {
+                    'stops': [
+                        [11, 15],
+                        [15, 20]
+                    ]
+                },
+                'heatmap-opacity': {
+                    'default': 1,
+                    'stops': [
+                        [14, 1],
+                        [15, 0]
+                    ]
+                }
+            }
+        },
+        'waterway-label'
+    );
+    setInterval(function () {
+        if (dataChanged) {
+            dataChanged = false
+            let city = document.getElementById("city").value
+            let start = document.getElementById("start").value
+            let end = document.getElementById("end").value
+            fetch(`http://127.0.0.1:5000/city_geo_json?city=${city}&start=${start}&end=${end}`)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    $(document).ready(function () {
+                        map.getSource('crime').setData(data);
+                        map.flyTo({
+                            center: data["features"][0]["geometry"]["coordinates"],
+                            essential: true
+                        });
+                    });
+                });
+        }
+    }, 50);
+});
 
 function createTable() {
     let table = document.createElement("table");
-    table.id="table";
+    table.id = "table";
     let header = document.createElement("thead");
     let row = document.createElement("tr");
 
     let city = document.createElement("th");
     city.setAttribute("data-field", "CITY_NAME");
-    city.innerText="City"
+    city.innerText = "City"
 
     let state = document.createElement("th");
     state.setAttribute("data-field", "STATE_NAME");
-    state.innerText="State"
+    state.innerText = "State"
 
     let code = document.createElement("th");
     code.setAttribute("data-field", "CRIME_CODE");
@@ -26,19 +113,19 @@ function createTable() {
 
     let dateReported = document.createElement("th");
     dateReported.setAttribute("data-field", "DATE_REPORTED");
-    dateReported.innerText="Date Reported"
+    dateReported.innerText = "Date Reported"
 
     let dateOccurred = document.createElement("th");
     dateOccurred.setAttribute("data-field", "DATE_OCCURRED");
-    dateOccurred.innerText="Date Occurred"
+    dateOccurred.innerText = "Date Occurred"
 
     let latitude = document.createElement("th");
     latitude.setAttribute("data-field", "LATITUDE");
-    latitude.innerText="Latitude"
+    latitude.innerText = "Latitude"
 
     let longitude = document.createElement("th");
     longitude.setAttribute("data-field", "LONGITUDE");
-    longitude.innerText="Longitude"
+    longitude.innerText = "Longitude"
 
     table.append(header)
     header.append(row)
@@ -57,21 +144,21 @@ pull.addEventListener("click", function () {
     let city = document.getElementById("city").value
     let start = document.getElementById("start").value
     let end = document.getElementById("end").value
-    console.log(city,start,end)
     fetch(`http://127.0.0.1:5000/city_crime_data?city=${city}&start=${start}&end=${end}`)
-    .then((response) => {
-        return response.json(); })
-  .then((data) => {
-      $(document).ready(function () {
-          console.log(data)
-          while (table_parent.firstChild) {
-            table_parent.removeChild(table_parent.firstChild);
-          }
-          let tableElement = createTable();
-          table_parent.append(tableElement);
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            $(document).ready(function () {
+                while (table_parent.firstChild) {
+                    table_parent.removeChild(table_parent.firstChild);
+                }
+                let tableElement = createTable();
+                table_parent.append(tableElement);
 
-          let table = document.getElementById("table");
-          $('table').bootstrapTable({ data: data.slice(0, 51) });
-      });
-  });
+                let table = document.getElementById("table");
+                $('table').bootstrapTable({data: data.slice(0, 51)});
+                dataChanged = true
+            });
+        });
 });
