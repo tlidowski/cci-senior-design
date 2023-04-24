1fa0077299648dc2912a69d012ce327ddb1af056
Sarar Aseer <sararaseer@sarars-mbp.mynetworksettings.com>
2023-04-24 01:31:36 -0400

    Remove non production code

--- api_calls/dc_parser.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def dc_crime_2022():
-    queryURL= "https://maps2.dcgis.dc.gov/dcgis/rest/services/FEEDS/MPD/MapServer/4/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-    response = requests.get(queryURL)
-    data = response.json()
-    print(data)
