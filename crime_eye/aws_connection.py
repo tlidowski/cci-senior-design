db93ff82ead8f498fe9b30eba420ef196bdf7d64
mzb35 <mzb35@drexel.edu>
2023-05-18 12:05:09 -0400

    Added Favicon

--- crime_eye/aws_connection.py
+++ crime_eye/aws_connection.py
@@ -13,7 +13,7 @@ def connectAWS():
     engine = psycopg2.connect(
         database=os.getenv("DATABASE_NAME"),
         user=os.getenv("DATABASE_USERNAME"),
-        password='',
+        password=input("Enter password: "),
         host=os.getenv("DATABASE_HOST"),
         port=os.getenv("DATABASE_PORT"),
     )
